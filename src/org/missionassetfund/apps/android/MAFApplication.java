b7d944ed886c2d334589cb58c1e81caa42220136
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-07-03 14:10:56 -0700

    Setup Parse enviroment

--- src/org/missionassetfund/apps/android/MAFApplication.java
+++ src/org/missionassetfund/apps/android/MAFApplication.java
@@ -1,13 +1,19 @@
+
 package org.missionassetfund.apps.android;
 
 import android.app.Application;
+
+import com.parse.Parse;
 
 public class MAFApplication extends Application {
 
     @Override
     public void onCreate() {
-        // TODO Auto-generated method stub
         super.onCreate();
+
+        // Initializing Parse
+        Parse.initialize(this, "YeCq7QZpwMg4vEqaZlJs2ZVZvQ7gJrTi1zN9aa4t",
+                "DgHFqxILVyV2qIGA0pLNrbMlNMQBqDIpI5PZjV9x");
     }
 
 }
