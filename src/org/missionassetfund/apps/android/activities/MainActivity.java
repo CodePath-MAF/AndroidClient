9b4fad348668be5fe7bcf062a8d1e396b3a0cd3f
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-07-06 22:22:44 -0700

    Create Dashboard Activity layout

--- src/org/missionassetfund/apps/android/activities/MainActivity.java
+++ src/org/missionassetfund/apps/android/activities/MainActivity.java
@@ -2,15 +2,54 @@
 package org.missionassetfund.apps.android.activities;
 
 import org.missionassetfund.apps.android.R;
+import org.missionassetfund.apps.android.fragments.DashboardFragment;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        
+        showDashboardFragment();
+    }
+    
+    private void showDashboardFragment() {
+        showFragment(DashboardFragment.class);
+    }
+    
+    @SuppressWarnings("rawtypes")
+    private void showFragment(Class activeFragmentClass) {
+        Class[] fragmentClasses = new Class[] {
+                DashboardFragment.class
+        };
+        FragmentManager mgr = getSupportFragmentManager();
+        FragmentTransaction transaction = mgr.beginTransaction();
+        try {
+            for (Class klass : fragmentClasses) {
+                Fragment fragment = mgr.findFragmentByTag(klass.getName());
+                if (klass == activeFragmentClass) {
+                    if (fragment != null) {
+                        transaction.show(fragment);
+                    } else {
+                        transaction.add(R.id.frmMainContent, (Fragment) klass.newInstance(),
+                                klass.getName());
+                    }
+                } else {
+                    if (fragment != null) {
+                        transaction.hide(fragment);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        transaction.commit();
     }
 }
