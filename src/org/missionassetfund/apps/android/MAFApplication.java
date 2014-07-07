f5e3911098b8b75955b5e609c4bd1365606f5875
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-07-07 13:45:19 -0700

    Model initial schema to Parse Objects

--- src/org/missionassetfund/apps/android/MAFApplication.java
+++ src/org/missionassetfund/apps/android/MAFApplication.java
@@ -1,15 +1,25 @@
 
 package org.missionassetfund.apps.android;
 
+import org.missionassetfund.apps.android.models.Goal;
+import org.missionassetfund.apps.android.models.Transaction;
+import org.missionassetfund.apps.android.models.User;
+
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MAFApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register the parse models
+        ParseObject.registerSubclass(User.class);
+        ParseObject.registerSubclass(Transaction.class);
+        ParseObject.registerSubclass(Goal.class);
 
         // Initializing Parse
         Parse.initialize(this, getString(R.string.parseApplicationId),
