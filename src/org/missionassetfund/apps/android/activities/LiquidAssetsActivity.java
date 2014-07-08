a76425eb2f3825c766f0d51bd6ab24bbef342a1f
Felipe Duarte <fcduarte@gmail.com>
2014-07-07 22:30:27 -0700

    Add Liquid Assets activity

---
+++ src/org/missionassetfund/apps/android/activities/LiquidAssetsActivity.java
@@ -0,0 +1,15 @@
+package org.missionassetfund.apps.android.activities;
+
+import org.missionassetfund.apps.android.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LiquidAssetsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_liquid_assets);
+	}
+}
