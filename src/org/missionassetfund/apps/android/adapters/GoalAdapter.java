7a243cb82577e0ffd613187ea14c658c1efd8141
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-07-20 19:24:50 -0700

    Polish Goal Items within the dashboard

--- src/org/missionassetfund/apps/android/adapters/GoalAdapter.java
+++ src/org/missionassetfund/apps/android/adapters/GoalAdapter.java
@@ -1,6 +1,9 @@
 
 package org.missionassetfund.apps.android.adapters;
 
+import java.util.Locale;
+import java.util.Random;
+
 import org.missionassetfund.apps.android.R;
 import org.missionassetfund.apps.android.models.Goal;
 import org.missionassetfund.apps.android.models.User;
@@ -42,6 +45,11 @@ public class GoalAdapter extends ParseQueryAdapter<Goal> {
         TextView tvName = (TextView) v.findViewById(R.id.tvName);
         TextView tvDueDate = (TextView) v.findViewById(R.id.tvDueDate);
         TextView tvPaymentDue = (TextView) v.findViewById(R.id.tvPaymentDue);
+        TextView tvMilestonesLeft = (TextView) v.findViewById(R.id.tvMilestonesLeft);
+
+        // reset view style
+        tvDueDate.setTextAppearance(getContext(), R.style.DashboardUI_GoalItem_DueDate);
+        tvDueDate.setCompoundDrawablesWithIntrinsicBounds(R.drawable.img_time_gray, 0, 0, 0);
 
         // Populate Goal Item
         tvName.setText(goal.getName());
@@ -50,19 +58,36 @@ public class GoalAdapter extends ParseQueryAdapter<Goal> {
         if (daysToDueDate > 1) {
             if (daysToDueDate < 7) {
                 tvDueDate.setText(getContext()
-                        .getString(R.string.goal_due_date_days, daysToDueDate));
+                        .getString(R.string.dashboard_goal_item_due_date_days, daysToDueDate)
+                        .toUpperCase(Locale.US));
+                setDueDateSoonStyle(tvDueDate);
             } else {
-                tvDueDate.setText(getContext().getString(R.string.goal_due_date,
-                        FormatterUtils.formatMonthDate(goal.getDueDate())));
+                tvDueDate.setText(getContext().getString(R.string.dashboard_goal_item_due_date,
+                        FormatterUtils.formatMonthDate(goal.getDueDate())).toUpperCase(Locale.US));
             }
         } else if (daysToDueDate == 1) {
-            tvDueDate.setText(getContext().getString(R.string.goal_due_date_tomorrow));
+            tvDueDate
+                    .setText(getContext().getString(R.string.dashboard_goal_item_due_date_tomorrow)
+                            .toUpperCase(Locale.US));
+            setDueDateSoonStyle(tvDueDate);
         } else {
-            tvDueDate.setText(getContext().getString(R.string.goal_due_date_today));
+            tvDueDate.setText(getContext().getString(R.string.dashboard_goal_item_due_date_today)
+                    .toUpperCase(Locale.US));
+            setDueDateSoonStyle(tvDueDate);
         }
 
         tvPaymentDue.setText(CurrencyUtils.getCurrencyValueFormatted(goal.getPaymentAmount()));
 
+        // TODO: set Number of milestone left base on Goal View logic
+        Random rand = new Random();
+        tvMilestonesLeft.setText(getContext().getString(R.string.dashboard_milestones_left_human,
+                rand.nextInt(8)).toUpperCase(Locale.US));
+
         return v;
+    }
+
+    private void setDueDateSoonStyle(TextView tvDueDate) {
+        tvDueDate.setTextAppearance(getContext(), R.style.DashboardUI_GoalItem_DueDateSoon);
+        tvDueDate.setCompoundDrawablesWithIntrinsicBounds(R.drawable.img_time_red, 0, 0, 0);
     }
 }
