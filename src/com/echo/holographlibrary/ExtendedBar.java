c7c52992617f5e8b8436e52c47d1b14838ccd239
Felipe Duarte <fcduarte@gmail.com>
2014-07-20 18:09:58 -0700

    Initial polish of the charts

---
+++ src/com/echo/holographlibrary/ExtendedBar.java
@@ -0,0 +1,14 @@
+package com.echo.holographlibrary;
+
+public class ExtendedBar extends Bar {
+    
+    private Object objectHolder;
+
+    public Object getObjectHolder() {
+        return objectHolder;
+    }
+
+    public void setObjectHolder(Object objectHolder) {
+        this.objectHolder = objectHolder;
+    }
+}
--- src/org/missionassetfund/apps/android/activities/LiquidAssetsActivity.java
+++ src/org/missionassetfund/apps/android/activities/LiquidAssetsActivity.java
@@ -2,15 +2,28 @@
 package org.missionassetfund.apps.android.activities;
 
 import org.missionassetfund.apps.android.R;
+import org.missionassetfund.apps.android.fragments.DailyTransactionsChartFragment.OnTransactionGroupClickedListener;
+import org.missionassetfund.apps.android.fragments.LiquidAssetsFragment;
+import org.missionassetfund.apps.android.models.TransactionGroup;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-public class LiquidAssetsActivity extends FragmentActivity {
+public class LiquidAssetsActivity extends FragmentActivity implements OnTransactionGroupClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liquid_assets);
+    }
+
+    @Override
+    public void onBarClicked(TransactionGroup transactionGroup) {
+        LiquidAssetsFragment liquidAssetsFragment = (LiquidAssetsFragment)
+                getSupportFragmentManager().findFragmentById(R.id.liquid_assets_fragment);
+
+        if (liquidAssetsFragment != null) {
+            liquidAssetsFragment.goToNextChart(transactionGroup);
+        }
     }
 }
