59613c8d3524eb8ab8d0657f514d87b2861d0893
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-08-04 11:57:33 -0700

    Make lending circle inner data density independent

--- src/org/missionassetfund/apps/android/fragments/PeopleCircleFragment.java
+++ src/org/missionassetfund/apps/android/fragments/PeopleCircleFragment.java
@@ -10,9 +10,6 @@ import org.missionassetfund.apps.android.adapters.ProfileCircularViewAdapter;
 import org.missionassetfund.apps.android.models.CashOutSchedule;
 import org.missionassetfund.apps.android.utils.CurrencyUtils;
 
-import com.lylc.widget.circularprogressbar.example.CircularProgressBar;
-import com.sababado.circularview.CircularView;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,6 +17,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lylc.widget.circularprogressbar.example.CircularProgressBar;
+import com.sababado.circularview.CircularView;
 
 public class PeopleCircleFragment extends Fragment {
 
@@ -29,13 +30,13 @@ public class PeopleCircleFragment extends Fragment {
     private BigDecimal mGoalPaymentAmount;
     private BigDecimal mGoalCurrentTotal;
     private List<CashOutSchedule> mCashOutSchedule;
-    
+
     private OnCreateViewListener listener;
-    
+
     public interface OnCreateViewListener {
         public void onSetupData(PeopleCircleFragment fragment);
     }
-    
+
     public void setGoalAmount(BigDecimal goalAmount) {
         this.mGoalAmount = goalAmount;
     }
@@ -43,7 +44,7 @@ public class PeopleCircleFragment extends Fragment {
     public void setGoalPaymentAmount(BigDecimal goalPaymentAmount) {
         this.mGoalPaymentAmount = goalPaymentAmount;
     }
-    
+
     public void setGoalCurrentTotal(BigDecimal goalCurrentTotal) {
         this.mGoalCurrentTotal = goalCurrentTotal;
     }
@@ -54,30 +55,35 @@ public class PeopleCircleFragment extends Fragment {
 
     @Override
     public void onAttach(Activity activity) {
-      super.onAttach(activity);
+        super.onAttach(activity);
         if (activity instanceof OnCreateViewListener) {
-          listener = (OnCreateViewListener) activity;
+            listener = (OnCreateViewListener) activity;
         } else {
-          throw new ClassCastException(activity.toString()
-              + " must implement PeopleCircleFragment.OnCreateViewListener");
+            throw new ClassCastException(activity.toString()
+                    + " must implement PeopleCircleFragment.OnCreateViewListener");
         }
     }
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable
-    ViewGroup container, @Nullable Bundle savedInstanceState) {
+    ViewGroup container, @Nullable
+    Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_people_circle, container, false);
         listener.onSetupData(this);
 
         cpbGoalDetail = (CircularProgressBar)
                 view.findViewById(R.id.cpbGoalDetail);
-        cpbGoalDetail.setTitle(CurrencyUtils.getCurrencyValueFormatted(mGoalAmount));
-        cpbGoalDetail.setSubTitle(String.format("%s CONTRIBUTED\nTHIS MONTH", 
-                CurrencyUtils.getCurrencyValueFormatted(mGoalCurrentTotal)));
+        TextView tvGoalAmount = (TextView) view.findViewById(R.id.tvGoalAmount);
+        TextView tvContributed = (TextView) view.findViewById(R.id.tvContributed);
         cpbGoalDetail.setProgress(getProgress());
         cpbGoalDetail.animateProgressTo(0, getProgress(), null);
 
-        ProfileCircularViewAdapter mAdapter = new ProfileCircularViewAdapter(view.getContext(), mCashOutSchedule);
+        tvGoalAmount.setText(CurrencyUtils.getCurrencyValueFormatted(mGoalAmount));
+        tvContributed.setText(String.format("%s\nCONTRIBUTED",
+                CurrencyUtils.getCurrencyValueFormatted(mGoalCurrentTotal)));
+
+        ProfileCircularViewAdapter mAdapter = new ProfileCircularViewAdapter(view.getContext(),
+                mCashOutSchedule);
         cvProfile = (CircularView) view.findViewById(R.id.cvProfile);
         cvProfile.animateHighlightedDegree(0, 360, 3000);
         cvProfile.setAdapter(mAdapter);
