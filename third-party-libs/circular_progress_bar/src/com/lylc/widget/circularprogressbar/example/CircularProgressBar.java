53572ad5c28cac01f96a09ce2e651322f33d420b
Felipe Duarte <fcduarte@gmail.com>
2014-08-03 19:02:21 -0700

    Fix CircularProgressBar error

--- third-party-libs/circular_progress_bar/src/com/lylc/widget/circularprogressbar/example/CircularProgressBar.java
+++ third-party-libs/circular_progress_bar/src/com/lylc/widget/circularprogressbar/example/CircularProgressBar.java
@@ -58,19 +58,27 @@ public class CircularProgressBar extends ProgressBar {
     private boolean mHasShadow = true;
     private int mShadowColor = Color.BLACK;
 
-    public interface ProgressAnimationListener {
+    public interface ProgressAnimationListener{
         public void onAnimationStart();
-
         public void onAnimationFinish();
-
         public void onAnimationProgress(int progress);
     }
 
-	public CircularProgressBar(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init(attrs, defStyle);
-	}
+    public CircularProgressBar(Context context) {
+        super(context);
+        init(null, 0);
+    }
 
+    public CircularProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public CircularProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+    
 	public void init(AttributeSet attrs, int style){
 		//so that shadow shows up properly for lines and arcs
 		setLayerType(View.LAYER_TYPE_SOFTWARE, null);
